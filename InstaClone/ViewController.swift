8ccd03703367ecbbd0c36bca13cd680f85b403f8
niles87 <nbing19@gmail.com>
2020-12-04 18:29:43 -0800

    added buttons to apply filters to img

--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -7,8 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let context = CIContext()
+    var original: UIImage!
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func sepia() {
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(0.75, forKey: kCIInputIntensityKey)
+        display(filter: filter!)
+    }
+    
+    @IBAction func noir() {
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        display(filter: filter!)
+    }
+    
+    @IBAction func vintage() {
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        display(filter: filter!)
+    }
 
+    func display(filter: CIFilter) {
+        guard let original = original else {
+            return
+        }
+        filter.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        let output = filter.outputImage
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+    }
+    
+    @IBAction func choosePhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            navigationController?.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
