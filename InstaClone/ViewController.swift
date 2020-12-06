420748ab3fc2afef5e2b1de0ccbf920f9b1db63a
niles87 <nbing19@gmail.com>
2020-12-06 10:58:50 -0800

    added invert filter and ability to save to photos

--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -14,33 +14,59 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet var imageView: UIImageView!
     
     @IBAction func sepia() {
+        guard original != nil else {
+            return
+        }
         let filter = CIFilter(name: "CISepiaTone")
         filter?.setValue(0.75, forKey: kCIInputIntensityKey)
         display(filter: filter!)
     }
     
     @IBAction func noir() {
+        guard original != nil else {
+            return
+        }
         let filter = CIFilter(name: "CIPhotoEffectNoir")
         display(filter: filter!)
     }
     
     @IBAction func vintage() {
+        guard original != nil else {
+            return
+        }
         let filter = CIFilter(name: "CIPhotoEffectProcess")
         display(filter: filter!)
     }
     
     @IBAction func invert() {
+        guard original != nil else {
+            return
+        }
         let filter = CIFilter(name: "CIColorInvert")
         display(filter: filter!)
     }
 
     func display(filter: CIFilter) {
-        guard let original = original else {
-            return
-        }
         filter.setValue(CIImage(image: original), forKey: kCIInputImageKey)
         let output = filter.outputImage
         imageView.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+        
+    }
+    
+    @IBAction func saveToPhotos(_ sender: AnyObject) {
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Save Error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true, completion: nil)
+        } else {
+            let ac = UIAlertController(title: "Saved Image!", message: "Altered image has been saved to photos", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true, completion: nil)
+        }
     }
     
     @IBAction func choosePhoto() {
@@ -59,12 +85,5 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             original = image
         }
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
 
